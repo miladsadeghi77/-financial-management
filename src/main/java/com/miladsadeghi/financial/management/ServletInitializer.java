247c04ac73ebde5fcb9950095951a76a6c2671f4
MiladSadeghi <miladpci88@gmail.com>
2024-10-16 18:51:09 +0330

    init project

---
+++ src/main/java/com/miladsadeghi/financial/management/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.miladsadeghi.financial.management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FinancialManagementApplication.class);
+    }
+
+}
